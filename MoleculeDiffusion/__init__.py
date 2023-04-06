891597402597185ecdc4950e8ea18dacf2e472ca
LAMM: MIT Laboratory for Atomistic and Molecular Mechanics <101393859+lamm-mit@users.noreply.github.com>
2023-04-06 09:26:08 -0400

    Add files via upload

---
+++ MoleculeDiffusion/__init__.py
@@ -0,0 +1,63 @@
+ 
+from MoleculeDiffusion.diffusion import (
+    ADPM2Sampler,
+    AEulerSampler,
+    Diffusion,
+    DiffusionInpainter,
+    DiffusionSampler,
+    Distribution,
+    KarrasSampler,
+    KarrasSchedule,
+    KDiffusion,
+    LinearSchedule,
+    LogNormalDistribution,
+    Sampler,
+    Schedule,
+    SpanBySpanComposer,
+    UniformDistribution,
+    VDiffusion,
+    VKDiffusion,
+    VKDistribution,
+    VSampler,
+    XDiffusion,
+)
+from MoleculeDiffusion.model import (
+    AudioDiffusionAE,
+    AudioDiffusionConditional,
+    AudioDiffusionModel,
+    AudioDiffusionUpphaser,
+    AudioDiffusionUpsampler,
+    AudioDiffusionVocoder,
+    DiffusionAE1d,
+    DiffusionAR1d,
+    DiffusionUpphaser1d,
+    DiffusionUpsampler1d,
+    DiffusionVocoder1d,
+    Model1d,
+)
+from MoleculeDiffusion.modules import NumberEmbedder, T5Embedder, UNet1d, XUNet1d
+
+from MoleculeDiffusion.utils import count_parameters 
+
+from MoleculeDiffusion.graphmodel import AnalogDiffusionSparse, AnalogDiffusionFull, pad_sequence  
+
+from MoleculeDiffusion.transformer import  pad_sequence, PositionalEncoding1D, PositionalEncodingPermute1D, MoleculeTransformer,MoleculeTransformerSequence
+
+from MoleculeDiffusion.modules import XUNet1d
+from MoleculeDiffusion.diffusion import ADPM2Sampler,XDiffusion_x,KDiffusion_mod,LogNormalDistribution
+from MoleculeDiffusion.diffusion import LinearSchedule, UniformDistribution, VSampler, XDiffusion,KarrasSchedule
+from MoleculeDiffusion.modules import STFT, SinusoidalEmbedding, XUNet1d, rand_bool, Encoder1d
+from MoleculeDiffusion.utils import (
+    closest_power_2,
+    default,
+    downsample,
+    exists,
+    groupby,
+    to_list,
+    upsample,
+)
+
+from MoleculeDiffusion.generative import pad_sequence_lastchannel, train_loop_forward, sample_loop_forward, predict_properties_from_SMILES, QMDiffusionForward, QMDiffusion, pad_sequence_end
+
+from MoleculeDiffusion.generative import is_novel, view_difference, draw_and_save, draw_and_save_set,MoleculeDataset, get_data_loaders, reverse_tokenize, train_loop_generative, sample_loop_generative
+
